7866409a60c59c0f18c692b1bd3d58e2cb2ac9da
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2022-02-04 19:51:07 +0530

    Tailwind setup.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Geralt Shopify Theme
 
+An upcoming theme for, well, Shopify!
 Base project - https://github.com/polidario/Elizabeth_Clean
+
+
+## Code
+
+`npx tailwind -i .\src\tailwind.css -o .\assets\tailwind.css --watch`
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./**/*.{liquid,json}"
+    // "./assets/*.liquid",
+    // "./layout/*.liquid",
+    // "./sections/*.liquid",
+    // "./snippets/*.liquid",
+    // "./templates/*.liquid",
+    // // "./templates/**/*.liquid"
+    // "./templates/customers/*.liquid"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
